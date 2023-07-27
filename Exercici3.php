4bb0d4592de809553db4c90a5f3570a480f2383a
AlejandroNPereyra <pereyra_alejandro@yahoo.com.ar>
2023-07-27 10:43:43 +0200

    + Correción Exercici3 función bool

--- Exercici3.php
+++ Exercici3.php
@@ -1,7 +1,8 @@
 <?php
 
 /* - Exercici 3
-Crea una funció que rebi com a paràmetres un array de paraules i un caràcter. La funció ens retorna true si totes les paraules de l’array tenen el caràcter passat com a segon paràmetre.
+Crea una funció que rebi com a paràmetres un array de paraules i un caràcter. 
+La funció ens retorna true si totes les paraules de l’array tenen el caràcter passat com a segon paràmetre.
 
 Per exemple:
 Si tenim [“hola”, “Php”, “Html”] retornarà true si preguntem per “h” però fals si preguntem per “l”.*/ 
@@ -11,15 +12,33 @@ $string2 = $_POST['string2'];
 $string3 = $_POST['string3'];
 $character = $_POST['character'];
 
-$wordsArray = array();
+/*$wordsArray = array();
 
 array_push($wordsArray, $string1);
 array_push($wordsArray, $string2);
 array_push($wordsArray, $string3);
 
-// var_dump($wordsArray);
+var_dump($wordsArray);*/
 
-foreach ($wordsArray as $strings) {
+function stringCheck ($string1, $string2, $string3, $character) {
+
+    if (strpos($string1, $character) !== false &&
+    strpos($string2, $character) !== false &&
+    strpos($string3, $character) !== false) {
+
+        return true;
+
+    } else {
+
+        return false;
+
+    }
+
+}
+
+var_dump(stringCheck($string1, $string2, $string3, $character));
+
+/*foreach ($wordsArray as $strings) {
 
     if (strpos($strings, $character) !== false) {
 
@@ -29,6 +48,7 @@ foreach ($wordsArray as $strings) {
 
         echo "The character '$character' doesn't exist in the string '$strings'.<br>";
     }
-}
+
+}*/
 
 ?>
